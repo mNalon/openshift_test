6860ae076fe058598040c63aa55e7fb179137895
Marcelo Nalon <marcelo.nalon@engenharia.ufjf.br>
2017-02-22 02:22:08 -0300

    ignore npm-debug.log

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var index = require('./routes');
+var cors = require('cors');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var httpStatus = require('http-status-codes');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.use('/category',require('./category'));
+
+/*
+  Handler error
+*/
+router.use((err, req, res, next)=>{
+
+  if(err.name === 'JsonSchemaValidation'){
+    return res.status(httpStatus.BAD_REQUEST).send('Corpo da mensagem inválida');
+  }
+
+  res.status(httpStatus.INTERNAL_SERVER_ERROR).send('Ocorreu um erro');
+
 });
 
 module.exports = router;
